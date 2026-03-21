d7f70aed876fc10eb1ca10f5246b6ce8880bfaa3
Lindisa <sbajisi@gmail.com>
2026-03-21 08:12:26 +0200

    Fix Terraform formatting

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,22 +1,22 @@
 module "cognito" {
-  source          = "../../modules/cognito"
-  user_pool_name  = "serverless-users"
-  client_name     = "serverless-client"
+  source         = "../../modules/cognito"
+  user_pool_name = "serverless-users"
+  client_name    = "serverless-client"
 }
 
 module "lambda" {
-  source           = "../../modules/lambda"
-  function_name    = "secure-api-lambda"
-  filename         = "../../lambda/function.zip"
-  lambda_role_arn  = "arn:aws:iam::123456789012:role/dummy-role"
+  source          = "../../modules/lambda"
+  function_name   = "secure-api-lambda"
+  filename        = "../../lambda/function.zip"
+  lambda_role_arn = "arn:aws:iam::123456789012:role/dummy-role"
 }
 
 module "api_gateway" {
-  source             = "../../modules/api_gateway"
-  lambda_invoke_arn  = module.lambda.invoke_arn
+  source            = "../../modules/api_gateway"
+  lambda_invoke_arn = module.lambda.invoke_arn
 }
 
 module "cloudfront" {
-  source           = "../../modules/cloudfront"
-  api_gateway_url  = module.api_gateway.invoke_url
+  source          = "../../modules/cloudfront"
+  api_gateway_url = module.api_gateway.invoke_url
 }
